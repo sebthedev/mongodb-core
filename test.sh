6a2d965c7083f05c190fa6344b77e65af1471647
Sebastian Hallum Clarke <s@zibity.com>
2017-07-05 17:42:55 -0400

    Trying to make tests work

--- test.sh
+++ test.sh
@@ -1,12 +1,16 @@
 echo "Running main tests"
 node test/runner.js -t functional
+killall mongod
 
 echo "Running auth tests"
 chmod 600 test/tests/functional/key/keyfile.key
 node test/runner.js -t functional -e auth
+killall mongod
 
 echo "Running replicaset tests"
 node test/runner.js -t functional -e replicaset
+killall mongod
 
 echo "Running sharded tests"
 node test/runner.js -t functional -e sharded
+killall mongod
