ce497d31cf3f910dd39acd6ae1364b97fb7a639d
Sebastian Hallum Clarke <s@zibity.com>
2017-07-05 17:48:56 -0400

    Tweaking test

--- test.sh
+++ test.sh
@@ -1,16 +1,12 @@
 echo "Running main tests"
 node test/runner.js -t functional
-killall mongod
 
 echo "Running auth tests"
 chmod 600 test/tests/functional/key/keyfile.key
 node test/runner.js -t functional -e auth
-killall mongod
 
 echo "Running replicaset tests"
 node test/runner.js -t functional -e replicaset
-killall mongod
 
 echo "Running sharded tests"
 node test/runner.js -t functional -e sharded
-killall mongod
