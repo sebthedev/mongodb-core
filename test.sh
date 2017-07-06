d7342e7ebbad8e3b94b48a92576ecfebf11f4cc8
Sebastian Hallum Clarke <s@zibity.com>
2017-07-06 14:48:05 -0400

    Not running replicaset and sharded tests

--- test.sh
+++ test.sh
@@ -5,8 +5,8 @@ echo "Running auth tests"
 chmod 600 test/tests/functional/key/keyfile.key
 node test/runner.js -t functional -e auth
 
-echo "Running replicaset tests"
-node test/runner.js -t functional -e replicaset
-
-echo "Running sharded tests"
-node test/runner.js -t functional -e sharded
+# echo "Running replicaset tests"
+# node test/runner.js -t functional -e replicaset
+#
+# echo "Running sharded tests"
+# node test/runner.js -t functional -e sharded
